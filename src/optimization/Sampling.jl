8ea78793ca1df3bad0f76b723a58812d0bec4aef
PastelBelem8 <catarina.belem@tecnico.ulisboa.pt>
2018-10-14 19:02:34 +0100

    [#13] Add box behnken sampling method

--- src/optimization/Sampling.jl
+++ src/optimization/Sampling.jl
@@ -83,9 +83,32 @@ function fullfactorial(ndims, level::Int=2)
     nbins = [0:step:1 for _ in 1:ndims]
     samples = fill(0.0, (ndims, level^ndims))
     for (n, bins) in enumerate(@strats(nbins))
-        samples[:, n] = [b for b in bins]
+        samples[:,n] = [b for b in bins]
     end
     samples
+end
+
+function boxbehnken(ndims)
+    if ndims < 3
+        throw(ArgumentError("invalid argument error. ndims $ndims < 3"))
+    end
+    # Block parameters
+    X0 = fullfactorial(2, 2)'
+    bsize = size(X0, 1)
+    nblines = convert(Int, 0.5*ndims*(ndims-1)) * bsize
+    # Create the samples for each combination
+    index = 0
+    X = zeros(nblines, ndims)
+    for i in 1:(ndims-1)
+        for j in (i+1):ndims
+            index = index + 1
+            X[(1+(index-1)*bsize):(index*bsize),1:end] .= 0.5
+            X[(1+(index-1)*bsize):(index*bsize),[i, j]] = X0
+             # X[(1+(index-1)*bsize):(index*bsize),1:end .!=i] = X0
+         end
+    end
+    # Append center
+    vcat(X, [0.5 for _ in 1:ndims]')
 end
 
 # Tests
@@ -135,6 +158,11 @@ fullfactorial(0, 3)
 fullfactorial(1, 3)
 fullfactorial(1, 1)
 
+boxbehnken(2)
+boxbehnken(3)
+boxbehnken(4)
+boxbehnken(5)
+
 
 end
 
@@ -142,3 +170,4 @@ end
 # [1] - Giunta, A. A., Wojtkiewicz, S., & Eldred, M. S. (2003).
 # Overview of modern design of experiments methods for computational
 # simulations. Aiaa, 649(July 2014), 6–9.
+# [2] - Box Behnken based on PyDOE2 implementation

0aea16c40e897fbdb5b138c75425fc4112d0b61a
Catarina Belém <pastelbelem8@Catarinas-MacBook-Pro.local>
2018-10-20 10:04:31 +0100

    Updated Platypus Integration module. Still requires a few redefinitions of methods. It is highly inspired on the Pandas.jl implementation and therefore needs to be adapted to the context of optimization

--- src/optimization/Platypus.jl
+++ src/optimization/Platypus.jl
@@ -1,121 +1,115 @@
 module Platypus
 
+using PyCall 
+using Base: show
 
-import Base: run
-
-export  CMAES,
-        EpsMOEA,
-        EvolutionaryStrategy,
-        GDE3,
-        GeneticAlgorithm,
-        IBEA,
-        MOEAD,
-        NSGAII,
-        NSGAIII,
-        OMOPSO,
-        PAES,
-        PESA2,
-        SMPSO,
-        SPEA2
-
-# Dependencies -----------------------------------------------------------
-using PyCall: PyObject, pycall, PyNULL
-
-# ------------------------------------------------------------------------
-# Initialization
-# ------------------------------------------------------------------------
 const platypus = PyNULL()
 
 function __init__()
-    copy!(platypus, pyimport_conda("platypus", "platypus-opt", "conda-forge"))
-end
-
-# ------------------------------------------------------------------------
-# 1. Define Problem
-# ------------------------------------------------------------------------
-# We need to define the Problem because the Python Algorithm receives a
-# Problem object. That must be produced.
-#
-#  Receives:
-#   - nvars
-#   - nobjs
-#   - nconstrs
-#   - function
-#   - contraints list of functions
-#   - continuous list of lower bounds and upper bounds
-#   - integer list of lower bounds and upper bounds
-#
-# Inherits from Platypus.Problem (has to call super)
-
-abstract type Problem end
-
-struct PlatypusProblem
-    pyo::PyObject
+   copy!(platypus, pyimport_conda("platypus", "platypus-opt", "conda-forge"))
 end
 
 
-# ------------------------------------------------------------------------
-# 2. Define the Algorithm
-# ------------------------------------------------------------------------
-# We can export the algorithms methods
-abstract type PlatypusAlgorithm end
+# Platypus Wrappers ---------------------------------------------------------
+# This implementation is inspired in Pandas.jl implementation. Using Python's 
+# metaprogramming capabilities we are able to mimic the existing data 
+# structures in Julia and provide a similar interface. 
+# ---------------------------------------------------------------------------
 
-PyCall.PyObject(x::PlatypusAlgorithm) = x.pyo
+# Wrapper type that gathers the behavior of Platypus entities
+abstract type PlatypusWrapped end
 
-macro pyAlgorithm(name)
-    quote
-        struct $(name) <: PlatypusAlgorithm
-            pyo::PyObject
-            $(esc(name))(pyo::PyObject) = new(pyo)
-            # Create the constructor
-            function $(esc(name))(args...; kwargs...)
-                new(pycall(platypus[$(QuoteNode(name))], PyObject, args...;kwargs...))
-            end
-        end
+PyCall.PyObject(x::PlatypusWrapped) = x.pyo
+
+# Create a dictionary to contain the PyObject->Julia_Wrapper type associations
+const pre_type_map = []
+const type_map = Dict{PyObject,Type}()
+
+"""
+  @pytype <name> <class>
+
+Creates the corresponding mutable Julia class and its constructors.
+"""
+macro pytype(name, class)
+  quote 
+    struct $(esc(name)) <: PlatypusWrapped
+      pyo::PyObject
+      $(esc(name))(pyo::PyObject) = new(pyo)
+  
+      # Create Constructor
+      function $(esc(name))(args...; kwargs...)                             
+        new(pycall(platypus[$(QuoteNode(name))], PyObject, args...;kwargs...))
+      end
+    end
+    # Associate PyObject <name> with the Julia Type <name>
+    push!(pre_type_map
+  end
+end
+
+
+"""
+  platypus_wrap(pyo::PyObject)
+
+Wraps an instance of platypus python class in the Julia type which corresponds 
+to that class. 
+"""
+function platypus_wrap(pyo::PyObject)
+  for (pyt, pyv) in type_map 
+    if pyisinstance(pyo, pyt)
+      return pyv(pyo)
+    end
+  end
+  convert(PyAny, pyo)
+end
+
+platypus_wrap(x::Union{AbstractArray, Tuple}) = [platypus_wrap(_) for _ in x]
+platypus_wrap(pyo) = pyo
+
+"""
+  pyattr(class, jl_method, py_method)
+
+ Uses Python's reflection capabilities to add methods to Python classes.
+"""
+function pyattr(class, jl_method, py_meth)
+  quote
+    function $(esc(jl_method))(pyt::$class, args...;kwargs...;)
+      #TODO - Fix the arguments (conversion to Python)
+      n_args = args 
+      method = pyt.pyo[$(string(py_method))]
+      pyo = pycall(method, PyObject, n_args...; kwargs...;)
+      wrapped = platypus_wrap(pyo)
     end
 end
 
+pyattr(class, method) = pyattr(class, method, method)
 
-@macroexpand @pyAlgorithm CMAES
-@pyAlgorithm EpsMOEA
-@pyAlgorithm EvolutionaryStrategy
-@pyAlgorithm GDE3
-@pyAlgorithm GeneticAlgorithm
-@pyAlgorithm IBEA
-@pyAlgorithm MOEAD
-@pyAlgorithm NSGAII
-@pyAlgorithm NSGAIII
-@pyAlgorithm OMOPSO
-@pyAlgorithm PAES
-@pyAlgorithm PESA2
-@pyAlgorithm SMPSO
-@pyAlgorithm SPEA2
-
-
-# TODO - Platypus Allows to define condition objects (MaxEvaluations and MaxTime)
-Base.run(a::PlatypusAlgorithm, condition::Int) = a.pyo[:run](condition)
-
-# FIXME - Update this when solution is wrapped
-function results(a::PlatypusAlgorithm)::AbstractMatrix
-    sols = a.pyo[:result]
-    nsols, ndims = length(sols), a.pyo[:problem][:nobjs]
-
-    res = zeros(Real, ndims, nsols)
-    feasible = fill(false, (nsols, 1))
-
-    for (j, sol) in enumerate(sols)
-        pyVector = convert(PyVector, sol[:objectives])
-
-        res[:,j] = Vector{Real}(pyvector)
-        feasible[j] = sol[:feasible]
-    end
-
-    res, feasible
+macro pyattr(class,  method)
+  pyattr(class, method)
 end
 
-function feasibleResults(a::PlatypusAlgorithm)::AbstractMatrix
-    results, feasibility = results(a)
-    results[feasibility]
+macro pyattr(class, method, orig_method)
+  pyattr(class, method, orig_method)
+end
+
+"""
+  pyattr_set(types, methods...)
+
+For each Julia Type `T<:PlatypusWrapped` in `types` and each method `m` 
+in `methods`, define a new function `m(t::T, args...)` that delegates 
+to the underlying pyobject wrapped by `t`. 
+"""
+function pyattr_set(classes, methods...)
+  for class in classes
+    for method in methods
+      @eval @pyattr($class, $method)
+    end
+  end
+end
+
+# Redefine custom printing methods
+function show(io::IO, pyv::PlatypusWrapped)
+  s = pyv.pyo[:__str__]()
+  println(io, s)
 end
 
 end # Module

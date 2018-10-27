d5e614543eb76488c9f978c0fd574277f56b73ed
PastelBelem8 <catarina.belem@tecnico.ulisboa.pt>
2018-10-27 11:09:33 +0100

    [#16] Implement Variable It could probably be a simpler... Maybe use a parametric type instead of creating different types. When doing refactory, please consider this.

--- src/optimization/Optimization.jl
+++ src/optimization/Optimization.jl
@@ -1,56 +1,102 @@
-# TODO - Define routines to process Callable Structures
-struct Callable <: Union{Function, Vector{Function}}
-    functions
-end
+# ----------------------------------------------------------------------
+# Utils
+# ----------------------------------------------------------------------
+# Routines to abstract and to the make code more readable/cleaner
 
-macro deftype(name, parentClass, fields...)
-    nameStr = string(name)
+# Fields
+parsefield(v::Expr, _=nothing) = :($(v.args[1])::$(v.args[2]))
+parsefield(v::Symbol, typ=nothing) = :($v::$typ)
 
-    # Assumes that every field has a default value
-    fieldNames = map(field->field.args[1], fields)
-    fieldTypes = map(field->field.args[2], fields)
+"Return base fields that comprise a data structure"
+function getbasefields(t::Type{Any}, typ::Type) end
 
-    # Make parameters
-    params = map(name ->  Expr(:kw, name), fieldNames)
-    constructorName = esc(name)
-    predicateName = esc(Symbol("is", nameStr, "?"))
-    structFields = map((name,typ) -> :($(name) :: $(typ)), fieldNames, fieldTypes)
+"Throws an error if the arguments of a certain type `T` are invalid."
+function checkArguments(t::Type{Any}, args...; kwargs...) end
 
-    quote
-        export $(name), $(predicateName)
-        struct $(esc(name)) <: $(esc(parentClass))
-            $(structFields...)
-        end
-        $(predicateName)(v::$(name)) = true
-        $(predicateName)(v::Any) = false
+"""
+    Variable Types
+"""
+abstract type  VarType end
+struct INT  <: VarType end
+struct SET  <: VarType end
+struct REAL <: VarType end
+
+const vartypes_map = Dict(  :INT => Int,
+                            :SET => Real,
+                            :REAL => Real )
+
+getVarType(v::Symbol) = haskey(vartypes_map, v) ? vartypes_map[v] : throw(ArgumentError("unexpected type ($v) does not have a matching Julia type."))
+getVarType(v::Type{T}) where T<:VarType = getVarType(Symbol(v))
+
+const sym2vartype_map = Dict( :INT => INT, :SET => SET, :REAL => REAL)
+
+# ---------------------------------------------------------------------
+# Variables
+# ---------------------------------------------------------------------
+# All variables share the same main behavior, however some discrete
+# variables require an additional field.
+#
+# Each variable has a domain/type associated. Either a variable is discrete
+# and in that case it might be a range of integers or a set of discrete
+# (real) numbers, or it might be continuous.
+
+abstract type AbstractVariable end
+
+getbasefields(t::Symbol, typ::Type) = [  esc(:(domain::$(Type{sym2vartype_map[t]}))), #FIXME
+                                         esc(:(lower_bound::$typ)),
+                                         esc(:(upper_bound::$typ)),
+                                         esc(:(initial_value::$typ))]
+
+function checkArguments(d, lb::T, ub::T, ival::T) where {T}
+    if !(typeof(lb) <: getVarType(d))
+        throw(TypeError("variable's domain type $d is not compliant with $T"))
+    elseif lb > ub
+        throw(ArgumentError("lower bound must be less than or equal to the upper bound: $lb ⩽ $ub"))
+    elseif lb > ival || ival > ub
+        throw(ArgumentError("the initial value must be within the lower and upper bounds: $lb ⩽ $ival ⩽ $ub"))
     end
 end
 
-@deftype(Variable,
-         Any,
-         values::Vector{Union{Real, Integer}},
-         lowerBound::Union{Real, Integer},
-         upperBound::Union{Real, Integer})
+macro defvariable(domain::Symbol, optional_fields...)
+    name_str = domain |> string |> titlecase |> x -> "$(x)Variable"
+    name_sym = Symbol(name_str)
+    varType = getVarType(domain)
 
-@deftype(Solution,
-         Any,
-         variables::AbstractVector{Variable},
-         objectives::AbstractVector{Float64},
-         isEvaluated::Bool,
-         isFeasible::Bool)
+    # Fields
+    base_fields = getbasefields(domain, varType)
+    optional_fields = map(field -> parsefield(field, varType), optional_fields)
 
-@deftype(Problem,
-         Any,
-         nvars::Int,
-         nobjs::Int,
-         nconstrs::Int,
-         func::Callable,
-         constraints::Callable,
-         continuous::Array{Real, 2},
-         integer::Array{Integer, 2})
+    # Make params
+    params = base_fields[2:end]
+    kw_params = map(field ->  Expr(:kw, field), optional_fields)
+    params_names = map(field -> esc(field.args[1].args[1]), params)
+    params_names = !isempty(kw_params) ? vcat(params_name, map(field -> field.args[1], optional_fields)) : params_names
 
-@deftype(Algorithm,
-         Any,
-         name::String,
-         problem::Problem,
-         maxEvals::Int)
+    # Methods
+    constructor_name = esc(name_sym)
+    predicate_name = esc(Symbol("is", name_str))
+
+    quote
+        struct $(name_sym) <: AbstractVariable
+            $(base_fields...)
+            $(optional_fields...)
+
+            function $(constructor_name)($(params...);$(kw_params...))
+                checkArguments($(esc(domain)), $(params_names...))
+                new($(esc(domain)), $(params_names...))
+            end
+        end
+
+        $(predicate_name)(v::AbstractVariable) = v.domain == $(esc(domain)) ? true : false
+        $(predicate_name)(v::Any) = false
+
+    end
+end
+
+@defvariable INT
+@defvariable SET
+@defvariable REAL
+
+# ---------------------------------------------------------------------
+# Objectives
+# ---------------------------------------------------------------------
